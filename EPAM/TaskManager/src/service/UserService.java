c2da7f725175111ffa27a4156b20b0025bfe0108
Dememedp <56649790+Dememedp@users.noreply.github.com>
2022-03-06 14:46:14 +0300

    Update UserService.java

--- EPAM/TaskManager/src/service/UserService.java
+++ EPAM/TaskManager/src/service/UserService.java
@@ -5,9 +5,12 @@ import service.exception.ServiceException;
 
 public interface UserService {
     String signIn(String login, String password) throws ServiceException;
+    
     void registration(String name, Role role, String login, String password) throws ServiceException;
 
     String showManagers() throws ServiceException;
 
     void deleteUser(String name) throws ServiceException;
+    
+    int count();
 }
